be218357035a6e40032a88c08ac8bf787652d9c9
Greg Garner <gpgarner10@gmail.com>
2016-12-04 20:38:15 -0500

    complete contact page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -10,8 +10,12 @@ export default Ember.Controller.extend({
   actions: {
 
     saveInvitation() {
-      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      const email = this.get('emailAddress');
+
+      const newInvitation = this.store.createRecord('invitation', { email: email });
+      newInvitation.save();
+
+      this.set('responseMessage', `Thank you! We have saved your email address: ${this.get('emailAddress')}`);
       this.set('emailAddress', '');
     }
   }
--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,23 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    var ENV = {
+      locationType: 'auto',
+      // ...
+      firebase: {
+        apiKey: 'AIzaSyDEa2dHCaobuIjJ8xYQQwn69OTO8-_gURY',
+        authDomain: 'emberlibrary-e9358.firebaseapp.com',
+        databaseURL: 'https://emberlibrary-e9358.firebaseio.com/',
+        storageBucket: 'emberlibrary-e9358.appspot.com',
+      },
+
+
+      // if using ember-cli-content-security-policy
+      contentSecurityPolicy: {
+        'script-src': "'self' 'unsafe-eval' apis.google.com",
+        'frame-src': "'self' https://*.firebaseapp.com",
+        'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+      },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
