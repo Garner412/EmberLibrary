7839e527e43e1a83f5ccf7b3cfa104ccc57563f5
Greg Garner <gpgarner10@gmail.com>
2016-12-05 01:44:55 -0500

    add delete button to admin contact and invitation

--- app/routes/admin/invitations.js
+++ app/routes/admin/invitations.js
@@ -4,5 +4,15 @@ export default Ember.Route.extend({
 
   model() {
     return this.store.findAll('invitation');
+  },
+
+  actions: {
+    deleteInvitation(invitation) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        invitation.destroyRecord();
+      }
+    }
   }
 });
