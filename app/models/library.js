7b2e457f2d25fbe767bc64a32c1513d8a60b497a
Greg Garner <gpgarner10@gmail.com>
2016-12-06 12:19:54 -0500

    create function for faking library data

--- app/models/library.js
+++ app/models/library.js
@@ -1,12 +1,25 @@
 import DS from 'ember-data';
 import Ember from 'ember';
+import Faker from 'faker';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   address: DS.attr('string'),
   phone: DS.attr('string'),
 
-  books: DS.hasMany('book'),
+  books: DS.hasMany('book', {inverse: 'library', async: true}),
 
-  isValid: Ember.computed.notEmpty('name')
+  isValid: Ember.computed.notEmpty('name'),
+
+  randomize(){
+    this.set('name', Faker.company.companyName() + ' Library');
+    this.set('address', this._fullAddress());
+    this.set('phone', Faker.phone.phoneNumber());
+
+    return this;
+  },
+
+  _fullAddress() {
+    return `${Faker.address.streetAddress()} + ${Faker.address.city()}`;
+  }
 });
