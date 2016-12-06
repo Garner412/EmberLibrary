427fd4b55b5d2ed614e5750d1dbbf057b082686e
Greg Garner <gpgarner10@gmail.com>
2016-12-06 13:59:05 -0500

    delete async: true from hasMany relationship

--- app/models/author.js
+++ app/models/author.js
@@ -4,7 +4,7 @@ import Faker from 'faker';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  books: DS.hasMany('book', {inverse: 'author', async: true}),
+  books: DS.hasMany('book', {inverse: 'author'}),
 
   isNotValid: Ember.computer.empty('name'),
 
