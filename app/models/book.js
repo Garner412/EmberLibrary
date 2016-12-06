213b6bee8deca1c5f5f4ba7bdd7760baeacf638c
Greg Garner <gpgarner10@gmail.com>
2016-12-06 12:29:28 -0500

    create functions for faking book models

--- app/models/book.js
+++ app/models/book.js
@@ -3,6 +3,29 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   releaseYear: DS.attr('date'),
-  library: DS.belongsTo('library'),
-  author: DS.belongsTo('author')
+
+  author: DS.belongsTo('author', inverse: 'books', async: true),
+  library: DS.belongsTo('library', inverse: 'books', async: true),
+
+  randomize(author, library) {
+    this.set('title', this._booktitle());
+    this.set('author', author);
+    this.set('releaseYear', this._randomYear());
+    this.set('library', library);
+
+    return this;
+  },
+
+  _booktitle() {
+    return `${Faker.commerce.productName()} Cookbook`;
+  },
+
+  _randomYear() {
+    return new Date(this._getRandomArbitrary(1900, 2016).toPrecision(4));
+  },
+
+  _getRandomArbitrary(min, max) {
+    return Math.random()
+  }
+
 });
